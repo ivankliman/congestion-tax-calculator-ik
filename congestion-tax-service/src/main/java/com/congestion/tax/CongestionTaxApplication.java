96834f6728c712da818e6507f6fafcd07bbbbfae
Ivan Kliman <ivan.kliman@hotmail.com>
2026-01-06 00:11:17 +0100

    Renaming app main class name

--- congestion-tax-service/src/main/java/com/congestion/tax/CongestionTaxApplication.java
+++ congestion-tax-service/src/main/java/com/congestion/tax/CongestionTaxApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CalculatorApplication {
+public class CongestionTaxApplication {
     public static void main(String[] args) {
-        SpringApplication.run(CalculatorApplication.class, args);
+        SpringApplication.run(CongestionTaxApplication.class, args);
     }
 }
